82de51a301d3fdaad9d5f370c0a270375cd9f75e
Angelica Ravelo <alu0100505092@ull.edu.es>
2014-11-04 23:44:05 +0100

    Lista enlazada..

--- lib/examen/lista.rb
+++ lib/examen/lista.rb
@@ -1,38 +1,50 @@
 
 
-Nodo =Struct.new(:value, :next)
+Nodo =Struct.new(:value, :next, :previous)
 
 
 
 class Lista
     def initialize(nodo)
         @inicio=Nodo.new(nil)
+        @fin=Nodo.new(nil)
     end
     
     
     def push(nodo)    #poner if preguntar Nodo vacio
         if @inicio!=nil #preguntar a COROMOTO
         nodo.next =@inicio
+        @inicio.previous =nodo
         #head=Nodo.new(nodo,@Inicio)
         @inicio = nodo
+        else 
+        @inicio = nodo
+        @fin = nodo
         end
+        
     end
     
     
     def pop(nodo)
         @inicio=@inicio.next
+        @inicio.previous=nil
     end
     
     def ext 
         aux=@inicio
         @inicio=@inicio.next
+        @fin=@fin.previous
         aux.value
     end
     
-    
+        
     def inicio
         
         @inicio
     end
     
+    def fin
+        
+        @fin
+    end
 end
